3665c0e7939bcb99f38b4ca1ad299e8bb870c1d1
Pohorielova <anna.pogorielova@gmail.com>
2022-12-05 16:51:22 +0100

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import shortid from 'shortid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  addName = name => {
+    console.log(name);
+    const contact = {
+      id: shortid.generate(),
+      name,
+    };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  handleChange = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.addName(this.state.name);
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+          <button type="submit">Save</button>
+        </form>
+
+        <ul>
+          {this.state.contacts.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+export default App;
